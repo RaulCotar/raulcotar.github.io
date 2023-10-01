788d3e4629a22336a3795c20ae67507aa1f5af2f
Raul Cotar <raulcotar00@gmail.com>
2023-10-01 15:32:41 +0200

    Cleanup

--- create_post.sh
+++ create_post.sh
@@ -3,4 +3,7 @@ if [[ $# -ne 1 ]] || [[ ! -f "$1" ]]; then
     echo 'Usage create_post <md_file_path>'
     return -1
 fi
-pandoc --from=commonmark_x --to=html --template='template/post.html' -o $(echo $1 | sed -r 's/(.*\/)?(.*)\..*/\2/' -).html $1
+pandoc --from=commonmark_x --to=html --template='template/post.html' \
+    --metadata "title=$(sed -En '{N; s/^(.*)\n===/\1/p ; D}' $1)" \
+    --metadata "date=$(date '+%d %b %Y')" \
+    -o $(echo $1 | sed -r 's/(.*\/)?(.*)\..*/\2/' -).html $1

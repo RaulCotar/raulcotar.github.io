923b83e7965c9cbef5604477170275d35b6a31a0
Raul Cotar <raulcotar00@gmail.com>
2023-10-12 20:52:57 +0200

    Small fix

--- src/create_post.sh
+++ src/create_post.sh
@@ -3,7 +3,7 @@ if [[ $# -ne 1 ]] || [[ ! -f "$1" ]]; then
     echo 'Usage: create_post <md_file_path>'
     return -1
 fi
-pandoc $1 --from=commonmark_x --to=html --template='src/post-template.html' \
+pandoc $1 --from=commonmark_x --to=html --template='post-template.html' \
     --metadata "date=$(date '+%d %b %Y')" \
     --metadata "date_machine=$(date '+%Y-%m-%d')" \
-    -o $(echo $1 | sed -r 's/(.*\/)?(.*)\..*/posts\/\2/' -).html
+    -o $(echo $1 | sed -r 's/(.*\/)?(.*)\..*/..\/posts\/\2/' -).html
